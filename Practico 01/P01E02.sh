e906310fe7dd248bace84a652e67ed0e9d022cb0
jisegura <segurajuanignacio@outlook.com>
2017-08-21 19:51:37 -0300

    add P01E02

---
+++ Practico 01/P01E02.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LIST=$( ls -lR $HOME )
+
+prompt_confirm() {
+	while true; do
+		read -r -p "$1 [y/n]: " REPLY
+		case $REPLY in
+			[yY][eE][sS]|[yY]) echo ; return 0 ;;
+			[nN][oO]|[nN]) echo ; return 1 ;;
+			*) echo "invalid input"
+		esac 
+	done  
+}
+
+if (( prompt_confirm "Desea comprimir como gzip" )); then
+	echo "$LIST" | gzip -cf > P01E02-Salida.gz
+else
+	echo "$LIST" > P01E02-Salida
+fi
